18f3643e1879443e5fd990fac17a65515467cd79
Uros Đurić <uros.djuric@asioso.com>
2020-11-08 09:26:06 +0100

    navigation finish

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,44 +1,93 @@
 import React, { Component } from 'react'
-import { faEnvelope, faBell, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faBell, faChevronDown, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './Navigation.scss'
-import logo from '../../assets/logo.png'
+import desktopLogo from '../../assets/logo.png'
+import mobileLogo from '../../assets/logo-mobile.png'
 import accImg from '../../assets/account_default.jpg'
 
 class Navigation extends Component {
+
+
+    constructor() {
+        super();
+        this.state = {};
+        this.isMobile();
+    }
+
+
+    isMobile = () => {
+        let width = window.innerWidth;
+        this.setState({
+            mobile: width < 900 ? true : false,
+        })
+    }
+
+
+    changeLogo = () => {
+        this.setState({
+            logo: this.state.mobile === true ? mobileLogo : desktopLogo
+        })
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", () => {
+            this.isMobile();
+            this.changeLogo();
+        });
+    }
+
+
+    componentWillUnmount() {
+        window.removeEventListener("resize");
+    }
+
+
+
     render() {
         return (
             <nav className="navigation" id="navigation">
                 <div className="navigation__logo">
-                    <a href="#">                    
-                        <img src={logo} alt="logo"/>
+                    <a href={void(0)}>                   
+                        <img src={this.state.logo} alt="logo"/>
                     </a>
+                </div>
+
+                <div className="navigation__form">
+                    <form action="">
+                        <input name="location" type="text" placeholder="Location"/>
+                        <input name="category" type="text" placeholder="Category"/>
+                        <input name="charity" type="text"  placeholder="Charity"/>
+                        <button>
+                        <FontAwesomeIcon icon={faSearch} />
+                        </button>
+                    </form>
                 </div>
                 
                 <div className="navigation__links">
                     <ul className="navigation__links__list">
                         <li className="navigation__links__list__item navigation__links__list__item--active">
-                            <a href="#">Search items</a>
+                            <a href={void(0)}>Search items</a>
                         </li>
                         <li className="navigation__links__list__item">
-                            <a href="#">Post items</a>
+                            <a href={void(0)}>Post items</a>
                         </li>
                     </ul>
                     <div className="navigation__links__account">
-                        <div className="navigation__links__account__out hide">
-                            <a href="#" className="button button--red">
+                        <div className="navigation__links__account__out">
+                            <a href={void(0)} className="button button--red">
                                 <span>Sign in</span>
                             </a>
-                            <a href="#" className="button button--white">
+                            <a href={void(0)} className="button button--white">
                                 <span>Sign up</span>
                             </a>
                         </div>
-                        <div className="navigation__links__account__in">
-                            <a href="#" className="navigation__links__account__in__icon">
+                        <div className="navigation__links__account__in hide">
+                            <a href={void(0)} className="navigation__links__account__in__icon">
                                 <FontAwesomeIcon icon={faEnvelope} />
                             </a>
-                            <a href="#" className="navigation__links__account__in__icon navigation__links__account__in__icon--alert">
+                            <a href={void(0)} className="navigation__links__account__in__icon navigation__links__account__in__icon--alert">
                                 <FontAwesomeIcon className="" icon={faBell} />
                                 <span>2</span>
                             </a>
@@ -61,6 +110,7 @@ class Navigation extends Component {
                         </div>
                     </div> 
                 </div>
+                
                 
             </nav>
         )
