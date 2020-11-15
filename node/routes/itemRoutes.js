855db1910f7e8c5f63f2536fc5f6304c9c0e2dd7
Uroš Djuric <interuros13@gmail.com>
2020-11-15 08:58:45 +0100

    refactoring

--- node/routes/itemRoutes.js
+++ node/routes/itemRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const itemController = require('../controllers/itemController');
 const router = express.Router();
 
-/* GET home page. */
+
 router.get('/list', itemController.item_list);
 /* router.get('/add', itemController.item_add); */
 
--- node/routes/userRoutes.js
+++ node/routes/userRoutes.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var userController = require("../controllers/userController")
 
-/* GET users listing. */
 router.post('/register', userController.user_register);
 router.post('/login', userController.user_login);
 router.get('/get', userController.user_get);
